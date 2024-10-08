7588f26914b00d1e887eb00c2a52233c00a34f3a
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-10-08 15:23:01 +0200

    Add log out button

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
-export default function Home() {
+import { redirect } from "next/navigation";
+import { auth, signOut } from "../../auth";
+
+export default async function Home() {
+    const session = await auth();
+    if (!session || !session.user) {
+        redirect('/login');
+    }
     return (
         <div>
             <h1>Hello, EESTEC!</h1>
+            <form action={async () => {
+                "use server";
+                await signOut();
+            }}>
+                <button>Sign Out</button>
+            </form>
         </div>
     );
 }
