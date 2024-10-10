3d7b3726a86a7ecfd3108c9fb4333e4671f346a6
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-10-10 13:38:10 +0200

    Add pages for project and new project

---
+++ src/app/projects/new/page.js
@@ -0,0 +1,7 @@
+export default async function NewProjectPage() {
+    return (
+        <div>
+            New project page
+        </div>
+    )
+}
---
+++ src/app/projects/page.js
@@ -0,0 +1,8 @@
+export default async function ProjectsPage() {
+    return (
+        <div>
+            <p>Projects page</p>
+            <a href="/projects/new">Add new project</a>
+        </div>
+    )
+}
