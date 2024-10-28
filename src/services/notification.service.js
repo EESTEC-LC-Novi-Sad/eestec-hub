58473cc5537ffc60c74e6ba6372f8482b0dfa307
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-10-28 17:08:55 +0100

    Add notification service

---
+++ src/services/notification.service.js
@@ -0,0 +1,34 @@
+import Notification from "../models/notification";
+import dbConnect from "@/app/lib/dbConnect";
+import User from "@/models/user";
+
+/**
+* @param {Object} notificationData
+* @param {String} notificationData.text
+* */
+async function createNotification(notificationData) {
+    await dbConnect();
+    const notification = await Notification.create({ ...notificationData });
+    return notification;
+}
+
+/**
+* @param {Object} notificationData
+* @param {String} notificationData.text
+* @param {String} userRole
+* */
+async function multicastNotification(notificationData, userRole) {
+    await dbConnect();
+    const notification = await createNotification(notificationData);
+    console.log(notification);
+
+    await User.updateMany(
+        { role: userRole },
+        { $push: { notifications: { notificationId: notification.id, isRead: false } } }
+    );
+}
+
+export {
+    multicastNotification
+}
+
