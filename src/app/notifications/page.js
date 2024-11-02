717373bb2db58cbeef4146f0497714faf737cb97
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-11-02 12:01:55 +0100

    Fetch notifications from db and not from session

--- src/app/notifications/page.js
+++ src/app/notifications/page.js
@@ -1,14 +1,17 @@
 import { getNotificationById } from "@/services/notification.service";
+import { getAllUserNotifications } from "@/services/user.service";
 import { auth } from "../../../auth"
 import Notification from "./notification";
 
 export default async function NotificationsPage() {
     const session = await auth();
+    const notificationIds = await getAllUserNotifications(session.user.id);
 
-    const notifications = await Promise.all(session.user?.notifications.map(async n => {
-        const notification = await getNotificationById(n.notificationId);
-        return notification;
-    }));
+    const notifications = await Promise.all(notificationIds
+        .map(async n => {
+            const notification = await getNotificationById(n.notificationId);
+            return notification;
+        }));
 
     return (
         <div>
