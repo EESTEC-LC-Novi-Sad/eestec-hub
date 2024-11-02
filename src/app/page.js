2b5f7ba66391963fc0f08d3c2cf8532e7acd0654
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-11-02 12:16:22 +0100

    Add project count to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { redirect } from "next/navigation";
 import { auth, signOut } from "../../auth";
 import { getAllUserNotifications } from "@/services/user.service";
+import { getProjectsCount } from "@/services/project.service";
 import Link from "next/link";
 
 export default async function Home() {
     const session = await auth();
     const notifications = await getAllUserNotifications(session.user.id);
+    const projectCount = await getProjectsCount();
     console.log(notifications);
     if (!session || !session.user) {
         redirect('/login');
@@ -15,6 +17,8 @@ export default async function Home() {
         <div>
             <h1>Hello, {session.user.firstName}</h1>
             <Link href="/notifications">My notifications: {notifications.length}</Link>
+            <br />
+            <Link href="/projects">Available projects: {projectCount}</Link>
             <form action={async () => {
                 "use server";
                 await signOut();
