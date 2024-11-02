479bdaf9f5f33c828a88c07ae1b37bd4b9ed6030
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-11-02 11:17:17 +0100

    Add all user notifications to the notification page

--- src/app/notifications/page.js
+++ src/app/notifications/page.js
@@ -1,7 +1,21 @@
+import { getNotificationById } from "@/services/notification.service";
+import { auth } from "../../../auth"
+import Notification from "./notification";
+
 export default async function NotificationsPage() {
+    const session = await auth();
+
+    const notifications = await Promise.all(session.user?.notifications.map(async n => {
+        const notification = await getNotificationById(n.notificationId);
+        return notification;
+    }));
+
     return (
         <div>
-            <h1>Notifications page</h1>
+            <h1>Notifications page</h1><br />
+            {notifications.map((n, index) => {
+                return <Notification key={index} notificationData={n} />
+            })}
         </div>
     )
 }
