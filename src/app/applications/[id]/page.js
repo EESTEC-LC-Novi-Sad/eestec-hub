ab6e95a2efbe70e18d0f6260a1c96a9a80760a4b
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-11-02 19:51:38 +0100

    Add logic to accept or reject an application

--- src/app/applications/[id]/page.js
+++ src/app/applications/[id]/page.js
@@ -1,6 +1,8 @@
-import { getApplicationById } from "@/services/application.service"
+import { setApplicationStatus, getApplicationById } from "@/services/application.service"
+import { sendNotificationById } from "@/services/notification.service";
 import { getProjectById } from "@/services/project.service";
 import { getUserById } from "@/services/user.service";
+import { redirect } from "next/navigation";
 
 export default async function ApplicationPage({ params }) {
     const application = await getApplicationById(params.id);
@@ -16,13 +18,38 @@ export default async function ApplicationPage({ params }) {
             <p><b>Motivational letter:</b></p>
             <p style={{ marginLeft: 50 }}>{application.motivationalLetter}</p>
             <br />
-            <form>
+
+            <form action={async () => {
+                "use server";
+
+                await setApplicationStatus(params.id, "accepted");
+                await sendNotificationById(member.id, {
+                    text: `Congratulations! You have been accepted to the ${project.name} project`,
+                    notificationType: "Application status",
+                    dateReceived: new Date(Date.now()),
+                    link: `/projects/${project.id}`
+                });
+                redirect("/applications");
+            }}>
                 <button><b>Accept {member.firstName}'s application</b></button>
             </form>
             <br />
-            <form>
-                <button><b>Decline {member.firstName}'s application</b></button>
+            <form action={async () => {
+                "use server";
+
+                await setApplicationStatus(params.id, "rejected");
+                await sendNotificationById(member.id, {
+                    text: `We are sorry! You have been rejected from the ${project.name} project`,
+                    notificationType: "Application status",
+                    dateReceived: new Date(Date.now()),
+                    link: `/projects/${project.id}`
+                });
+                redirect("/applications");
+            }}>
+                <button><b>Reject {member.firstName}'s application</b></button>
             </form>
         </div>
     )
+
+    //TODO: make accept and decline buttons work! Before that, alter project model to hold this info
 }
