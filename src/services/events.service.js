e763db2d6f150d4c71c32722c595916d13379866
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-11-29 15:23:11 +0100

    Add a service for events

---
+++ src/services/events.service.js
@@ -0,0 +1,23 @@
+import dbConnect from "@/app/lib/dbConnect";
+import Event from "@/models/event";
+
+/**
+* @param {{name: String, description: String, startDate: Date, endDate: Date, location: String, code: String}} eventData
+* */
+async function createEvent(eventData) {
+    await dbConnect();
+    const event = Event.create({ ...eventData });
+    return event;
+}
+
+async function getAllEvents() {
+    await dbConnect();
+    const events = Event.find();
+    return events;
+}
+
+export {
+    createEvent,
+    getAllEvents
+}
+
