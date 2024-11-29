3af4f5d272154e7871d24bf7b43824b291f98282
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-11-29 15:24:07 +0100

    Add location to eventData for jsdoc

--- src/services/events.service.js
+++ src/services/events.service.js
@@ -2,7 +2,7 @@ import dbConnect from "@/app/lib/dbConnect";
 import Event from "@/models/event";
 
 /**
-* @param {{name: String, description: String, startDate: Date, endDate: Date, location: String, code: String}} eventData
+* @param {{name: String, description: String, startDate: Date, endDate: Date, location: String, code: String, attendees: []}} eventData
 * */
 async function createEvent(eventData) {
     await dbConnect();
