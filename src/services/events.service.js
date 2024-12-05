367521b74e21f7af8ad64b4a39b532f0464ef30d
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-12-05 11:52:03 +0100

    Add findById and update methods to event service

--- src/services/events.service.js
+++ src/services/events.service.js
@@ -1,23 +1,44 @@
 import dbConnect from "@/app/lib/dbConnect";
 import Event from "@/models/event";
+import User from "@/models/user";
 
 /**
 * @param {{name: String, description: String, startDate: Date, endDate: Date, location: String, code: String, attendees: []}} eventData
 * */
 async function createEvent(eventData) {
     await dbConnect();
-    const event = Event.create({ ...eventData });
+    const code = Math.round(Math.random() * 100000);
+    const event = Event.create({ ...eventData, code });
     return event;
 }
 
 async function getAllEvents() {
     await dbConnect();
-    const events = Event.find();
+    const events = await Event.find();
     return events;
+}
+
+async function getEventById(id) {
+    await dbConnect();
+    const event = await Event.findById(id);
+    return event;
+}
+
+async function joinEvent(eventId, userId) {
+    await dbConnect();
+    const user = await User.findById(userId);
+    if (!user) {
+        console.log(`User with id=${userId} doesn't exist`);
+    }
+
+    const event = await Event.findByIdAndUpdate(eventId, { $addToSet: { attendees: user.id } });
+    return event;
 }
 
 export {
     createEvent,
-    getAllEvents
+    getAllEvents,
+    getEventById,
+    joinEvent
 }
 
