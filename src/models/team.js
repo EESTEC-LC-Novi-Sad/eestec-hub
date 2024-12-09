ee036e62aaee3a0bc26f66579f775d209d1ac9de
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-12-09 14:07:39 +0100

    Add team model

---
+++ src/models/team.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const teamSchema = new Schema({
+    name: String,
+    description: String,
+    teamMembers: [{ type: Schema.Types.ObjectId }]
+});
+
+const Team = mongoose.models.Team || mongoose.model('Team', teamSchema);
+
+export default Team;
