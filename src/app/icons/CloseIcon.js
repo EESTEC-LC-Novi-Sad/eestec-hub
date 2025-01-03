3ba9b9ffea8945e7afd25edc1340b7fe054d3b0c
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2025-01-03 21:14:44 +0100

    Add icons for menu and header

---
+++ src/app/icons/CloseIcon.js
@@ -0,0 +1,3 @@
+export default function CloseIcon() {
+  return <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><g fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path d="M8 8L40 40"/><path d="M8 40L40 8"/></g></svg>
+}
