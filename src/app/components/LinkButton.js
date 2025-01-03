bc2565f7a4df2e53538395eb7b8809a81d191a15
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2025-01-03 23:36:18 +0100

    Add link component looking like a button

---
+++ src/app/components/LinkButton.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function LinkButton({href, children, className}) {
+  return <div>
+    <Link  href={href} passHref legacyBehavior>
+      <a className={`inline-block p-1 border rounded border-solid border-gray-400 hover:bg-gray-200 ${className}`}>{children}</a>
+    </Link>
+   </div>
+  
+}
+
