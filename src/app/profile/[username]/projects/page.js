94006863d85252698ff427211cf0be9ae95ee7ac
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2025-01-09 17:15:46 +0100

    Add page to display personal projects

---
+++ src/app/profile/[username]/projects/page.js
@@ -0,0 +1,31 @@
+import { getAllProjectsByUsername } from "@/services/project.service";
+import Link from "next/link";
+import { auth } from "../../../../../auth";
+import { redirect } from "next/navigation";
+
+export default async function UserProjectPage({params}) {
+    const session = await auth();
+    if (!session || !session.user) {
+        redirect("/login");
+    }
+
+    const { username } = params;
+    const userProjects = await getAllProjectsByUsername(username);
+
+    return (
+        <div>
+            <div>These are your projects</div>
+            {
+                userProjects.map((project, index) => {
+                    return <div key={index}>
+                        <p><b>{project.name}</b></p>
+                        <Link href={`/projects/${project.id}`}>Go to project details</Link><br />
+                        <Link href={`/projects/${project.id}/apply`}>Apply for project</Link><br />
+                        <Link href={`/projects/${project.id}/applications`}>Go to applications</Link>
+                        <br /><br />
+                    </div>
+                })
+            }
+        </div>
+    )
+}
