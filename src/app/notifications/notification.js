889b3674aefc66c763a0bec9bcd0eec250a61ab1
mihna123 <mihailonvojinovic@gmail.com>
2025-01-16 22:42:30 +0100

    Add jsdoc to notification component

--- src/app/notifications/notification.js
+++ src/app/notifications/notification.js
@@ -1,7 +1,21 @@
 import Link from "next/link";
 
+/**
+* @typedef NotificationData
+* @property {String} text
+* @property {String} notificationType
+* @property {Date} dateReceived
+* @property {String} link
+* @property {Boolean} isChecked
+*
+*
+*
+* @param {Object} param0
+* @param {NotificationData} param0.notificationData
+* */
 export default function Notification({ notificationData }) {
-    return <div>
+    return <div className="border border-black flex" >
+        <input type="checkbox" checked={notificationData.isChecked}/>
         <Link href={notificationData.link}>{notificationData.text}</Link>
     </div>
 }
