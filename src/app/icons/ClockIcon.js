2368d6eaebf65d3b897239a2fdab17385791aae0
mihna123 <mihailonvojinovic@gmail.com>
2025-01-23 16:23:24 +0100

    Add new clock icon

---
+++ src/app/icons/ClockIcon.js
@@ -0,0 +1,3 @@
+export default function ClockIcon({className}) {
+    return <svg className={className} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"><circle cx="8" cy="8" r="6.25"/><path d="M8.25 4.75v3.5l-2.5 2"/></g></svg>
+}
