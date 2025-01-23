32e2af79cea0d0ecee9d75ef7bf8ac2a42e8e463
mihna123 <mihailonvojinovic@gmail.com>
2025-01-23 16:23:41 +0100

    Update ui for event pages

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,28 +1,56 @@
 import { getAllEvents } from "@/services/events.service";
 import { auth } from "../../../auth";
 import Link from "next/link";
+import Tag from "../components/Tag";
+import ClockIcon from "../icons/ClockIcon";
+import { mapEventToCard } from "../lib/utils";
+
+function Separator() {
+    return <div className="border-t border-gray-300 my-2"></div>
+}
 
 export default async function EventsPage() {
     const session = await auth();
     if (!session || !session.user) {
         redirect('/login');
     }
-    const events = await getAllEvents()
+    const events = (await getAllEvents()).sort((a, b) => b.startDate - a.startDate);
+    const eventYears = [...new Set(events.map(e => e.startDate.getFullYear()))];
+
+    const upcomingEvents = events.filter(e => e.startDate >= new Date());
+    const pastEvents = events.filter(e => e.startDate < new Date());
+
+    const upcomingEventsByYear = eventYears.map(year => upcomingEvents.filter(e => e.startDate.getFullYear() === year));
+    const pastEventsByYear = eventYears.map(year => pastEvents.filter(e => e.startDate.getFullYear() === year));
+
     return (
-        <div>
-            <h1>Events page</h1>
-            {
-                events.map((e, index) => {
-                    return <div key={index}>
-                        <Link href={`events/${e.id}`}>
-                            <p>
-                                <b>{e.name}</b> - starts at {" "}
-                                {e.startDate.toLocaleString('en-GB', { timeZone: 'UTC' })}
-                            </p>
-                        </Link>
-                    </div>
-                })
-            }
+        <div className="flex justify-center">
+            <div className="w-full md:w-7/12">
+                <h1 className="text-2xl"><b>Upcoming events</b></h1>
+                { !upcomingEvents || upcomingEvents.length === 0 
+                    ? <p className="ml-4 text-gray-700 text-sm">No upcoming events</p>
+                    : <div>{ upcomingEventsByYear.map((year, index) => {
+                        return <div key={`year-${index}`}>
+                            <h1 className="text-center text-3xl mt-2"><b>{year[0].startDate.getFullYear()}</b></h1>
+                            {year.map(mapEventToCard)}
+                        </div>
+                })}</div>
+                }
+                <Separator/>
+                <h1 className="text-2xl"><b>Past events</b></h1>
+                { !pastEvents || pastEvents.length === 0 
+                    ? <p className="ml-4 text-gray-700 text-sm">No past events</p>
+                    : <div>{ pastEventsByYear.map((year, index) => {
+                        return <div key={`year-${index}`}>
+                            <h1 className="text-center text-3xl mt-2"><b>{year[0].startDate.getFullYear()}</b></h1>
+                            {year.map(mapEventToCard)}
+                        </div>
+                })}</div>
+                }
+            </div>
         </div>
     )
 }
+
+
+// {event.startDate.toLocaleString('en-GB', { timeZone: 'UTC' })}
--- src/app/profile/[username]/events/page.js
+++ src/app/profile/[username]/events/page.js
@@ -1,6 +1,7 @@
 import { auth } from "../../../../../auth";
 import { redirect } from "next/navigation";
 import { getAllEventsByUsername} from "@/services/events.service";
+import { mapEventToCard } from "../../../lib/utils";
 import Link from "next/link";
 
 export default async function EventsByUsernamePage({params}) {
@@ -10,21 +11,25 @@ export default async function EventsByUsernamePage({params}) {
     }
 
     const { username } = params;
-    const userEvents = await getAllEventsByUsername(username);
-    return (
-        <div>
-            <h1>Events you attended</h1>
-            { !userEvents || userEvents.length === 0
-                ? <p>You have not attended any events</p>
-                : userEvents.map((e, index) => <div key={index}>
-                <Link href={`/events/${e.id}`}>
-                    <p>
-                        <b>{e.name}</b> - starts at {" "}
-                        {e.startDate.toLocaleString('en-GB', { timeZone: 'UTC' })}
-                    </p>
-                </Link>
-            </div>)}
+    const userEvents = (await getAllEventsByUsername(username)).sort((a, b) => b.startDate - a.startDate);
+    const eventYears = [...new Set(userEvents.map(e => e.startDate.getFullYear()))];
+    const eventsByYear = eventYears.map(year => userEvents.filter(e => e.startDate.getFullYear() === year));
 
+    return (
+        <div className="flex justify-center">
+            <div className="w-full md:w-7/12">
+                <h1 className="text-2xl"><b>Events you attended</b></h1>
+                { !userEvents || userEvents.length === 0
+                    ? <p>You have not attended any events</p>
+                    : <div>{ eventsByYear.map((year, index) => {
+                        return <div key={`year-${index}`}>
+                            <h1 className="text-center text-3xl mt-2"><b>{year[0].startDate.getFullYear()}</b></h1>
+                            {year.map(mapEventToCard)}
+                        </div>
+                    })}
+                    </div>
+                }
+            </div>
         </div>
     )
 }
