e5da7d9a75646052ef2d5757a3c465d16d4276e6
mihna123 <mihailonvojinovic@gmail.com>
2025-01-23 14:28:01 +0100

    Improve ui for teams pages

--- src/app/profile/[username]/teams/page.js
+++ src/app/profile/[username]/teams/page.js
@@ -2,6 +2,8 @@ import { getAllUserTeams } from "@/services/team.service";
 import { auth } from "../../../../../auth";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import Tag from "../../../components/Tag";
+import Button from "../../../components/Button";
 
 export default async function UserTeamsPage({params}) {
     const session = await auth();
@@ -13,19 +15,39 @@ export default async function UserTeamsPage({params}) {
     const userTeams = await getAllUserTeams(username);
 
     return (
-        <div>
-            <div>Your teams</div>
-            { !userTeams || userTeams.length === 0
-                ? <p>You are not in any teams</p>
-                : userTeams.map((team, index) => {
-                return (
-                    <Link key={index} href={`/teams/${team.id}`}>
-                        <div>
-                            <p>{team.name}</p>
-                        </div>
-                    </Link>
-                )
-            })}
+        <div className="flex justify-center">
+            <div className="w-full md:w-8/12">
+                <h1 className="text-2xl ml-2 mb-4"><b>Your teams</b></h1>
+                <div className="border border-gray-300 md:rounded  divide-y divide-gray-300 mb-4">
+                    <div>
+                    { !userTeams || userTeams.length === 0
+                        ? <p>You are not in any teams</p>
+                        : userTeams.map(mapTeamsToCards)}
+                    </div>
+                </div>
+                <Link href="/teams/" className="text-blue-700 hover:underline mx-4">Join more teams</Link>
+            </div>
         </div>
     )
+}
+
+/**
+* @param {Number} index 
+* @param {{name: String, description: String, teamMembers: ObjectId}} team 
+* */
+function mapTeamsToCards(team, index) {
+    return (
+        <div key={`team-${index}`} className="flex justify-between py-4">
+            <div className="flex items-center">
+                <Link className="text-blue-700 hover:underline mx-4" href={`/teams/${team.id}`}>
+                    {team.name}
+                </Link>
+                <Tag className="text-gray-600 border-gray-400">Member</Tag>
+            </div>
+            <div className="flex items-center">
+                <Button className="mr-4">Settings</Button>
+                <Button className="mr-4 text-red-600 hover:text-red-800">Leave</Button>
+            </div>
+        </div>
+    );
 }
--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -1,19 +1,36 @@
 import { getAllTeams } from "@/services/team.service"
 import Link from "next/link";
+import Tag from "../components/Tag";
+import Button from "../components/Button";
 
 export default async function TeamsPage() {
     const teams = await getAllTeams();
     return (
-        <div>
-            {teams.map((team, index) => {
-                return (
-                    <Link key={index} href={`/teams/${team.id}`}>
-                        <div>
-                            <p>{team.name}</p>
-                        </div>
-                    </Link>
-                )
-            })}
+        <div className="flex flex-col items-center md:mt-4">
+            <div className="md:border border-gray-300 md:rounded w-full md:w-8/12 divide-y divide-gray-300">
+                {teams.map(mapTeamsToCards)}
+            </div>
         </div>
     )
 };
+
+/**
+* @param {Number} index 
+* @param {{name: String, description: String, teamMembers: ObjectId}} team 
+* */
+function mapTeamsToCards(team, index) {
+    return (
+        <div key={`team-${index}`} className="flex justify-between py-4">
+            <div className="flex items-center">
+                <Link className="text-blue-700 hover:underline mx-4" href={`/teams/${team.id}`}>
+                    {team.name}
+                </Link>
+                <Tag className="text-gray-600 border-gray-400">Member</Tag>
+            </div>
+            <div className="flex items-center">
+                <Button className="mr-4">Settings</Button>
+                <Button className="mr-4 text-red-600 hover:text-red-800">Leave</Button>
+            </div>
+        </div>
+    );
+}
