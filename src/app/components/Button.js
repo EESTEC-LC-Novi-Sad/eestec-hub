e25cfa009cb5d086ba821c9c50c97169894695e8
mihna123 <mihailonvojinovic@gmail.com>
2025-01-25 16:11:07 +0100

    Remove text color change when hover on button

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -3,7 +3,6 @@ export default function Button({ children, className, onClick, type }) {
       <button type={type} onClick={onClick} className={
           `p-1 border border-solid border-gray-400 rounded 
            hover:bg-gray-200
-           hover:text-gray-700
            ${className}`
       }>
         {children} 
