7af232ed4a59ac946bbc9c878808328f951ec4e8
mihna123 <mihailonvojinovic@gmail.com>
2025-01-26 16:38:00 +0100

    Add config uri for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "dc3lqfwgkms4bzfx.public.blob.vercel-storage.com",
+                port: ""
+            }
+        ]
+    }
+};
 
 export default nextConfig;
