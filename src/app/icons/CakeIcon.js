6426d0ee370cae36c4e21bb56344245dc14a2789
mihna123 <mihailonvojinovic@gmail.com>
2025-01-27 19:51:12 +0100

    Add more icons

---
+++ src/app/icons/CakeIcon.js
@@ -0,0 +1,3 @@
+export default function CakeIcon({className}) {    
+    return <svg className={className} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M20 21v-8a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8"/><path d="M4 16s.5-1 2-1s2.5 2 4 2s2.5-2 4-2s2.5 2 4 2s2-1 2-1M2 21h20M7 8v2m5-2v2m5-2v2M7 4h.01M12 4h.01M17 4h.01"/></g></svg>;
+}
