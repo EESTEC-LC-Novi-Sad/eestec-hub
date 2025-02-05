3c82e5665b202eb5cb0cc1033bfdd28445062aef
mihna123 <mihailonvojinovic@gmail.com>
2025-02-05 17:44:58 +0100

    Add error handling to apply for project method

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -57,26 +57,32 @@ async function getProjectById(id) {
 }
 
 async function applyToProject(projectId, applicationData) {
-    const session = await auth();
-    if (!session || !session.user) {
-        throw new Error("User must be logged in to apply!");
+    try {
+        const session = await auth();
+        if (!session || !session.user) {
+            throw new Error("User must be logged in to apply!");
+        }
+        await dbConnect();
+
+        const { motivationalLetter, position } = applicationData;
+
+        const newApplication = await Application.create({
+            memberId: session.user.id,
+            projectId,
+            motivationalLetter,
+            position
+        });
+
+        await Project.findByIdAndUpdate(
+            projectId,
+            { $push: { applications: newApplication.id } },
+            { new: true }
+        );
+        return true;
+    } catch(e) {
+        console.error(e)
+        return false;
     }
-    await dbConnect();
-
-    const { motivationalLetter, position } = applicationData;
-
-    const newApplication = await Application.create({
-        memberId: session.user.id,
-        projectId,
-        motivationalLetter,
-        position
-    });
-
-    await Project.findByIdAndUpdate(
-        projectId,
-        { $push: { applications: newApplication.id } },
-        { new: true }
-    );
 }
 
 async function getApplicationsForProject(projectId) {
