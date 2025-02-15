cbe337cceaae5dfceb806e8b02bf1ef7d60440a5
mihna123 <mihailonvojinovic@gmail.com>
2025-02-15 23:42:08 +0100

    Update event page ui

---
+++ src/app/events/[id]/ShowCodeButton.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import Button from "../../components/Button";
+
+export default function ShowCodeButton({ secretCode }) {
+	const [isShown, setIsShown] = useState(false);
+
+	const showCode = () => {
+		setIsShown(true);
+	};
+
+	const closeCode = () => {
+		setIsShown(false);
+	};
+	return (
+		<div>
+			{isShown && (
+				<div
+					onClick={closeCode}
+					className="absolute left-0 top-0 z-50 w-screen h-screen bg-gray-900/50 flex justify-center"
+				>
+					<div className="bg-white flex flex-col items-center py-4 h-fit mt-32 rounded w-96">
+						<p className="text-2xl">The secret event code is </p>
+						<b className="text-5xl">{secretCode}</b>
+						<Button onClick={closeCode} className="mt-4">
+							Close
+						</Button>
+					</div>
+				</div>
+			)}
+			<Button onClick={showCode}>Show event code</Button>
+		</div>
+	);
+}
--- src/app/events/[id]/page.js
+++ src/app/events/[id]/page.js
@@ -1,29 +1,59 @@
 import { getEventById } from "@/services/events.service";
+import { redirect } from "next/navigation";
+import { auth } from "../../../../auth";
 import Link from "next/link";
+import Tag from "../../components/Tag";
+import LinkButton from "../../components/LinkButton";
+import Button from "../../components/Button";
+import TeamsIcon from "../../icons/TeamsIcon";
+import ClockIcon from "../../icons/ClockIcon";
+import EventsIcon from "../../icons/EventsIcon";
+import ShowCodeButton from "./ShowCodeButton";
+import { Separator } from "@/app/lib/utils";
 
 export default async function EventByIdPage({ params }) {
+	const session = await auth();
+	if (!session || !session.user) {
+		redirect("/login");
+	}
+
+	const isBoard = session.user.role === "board";
+
 	const event = await getEventById(params.id);
+
 	return (
-		<div>
-			<p>
-				<b>Event name:</b> {event.name}
-			</p>
-			<p>
-				<b>Event description:</b> {event.description}
-			</p>
-			<p>
-				<b>Event start date:</b> {event.startDate.toLocaleString("en-GB")}
-			</p>
-			<p>
-				<b>Event end date:</b> {event.endDate.toLocaleString("en-GB")}
-			</p>
-			<p>
-				<b>Event join code:</b> {event.code} (hide this for members)
-			</p>
-			<p>
-				<b>Event attendees:</b> {event.attendees.length}
-			</p>
-			<Link href={`/events/${event.id}/join`}>Click here to join event</Link>
+		<div className="flex justify-center px-2">
+			<div className="w-full md:w-6/12">
+				<h1 className="text-3xl my-2">
+					<b>{event.name}</b>
+				</h1>
+				<Separator />
+				<span className="flex gap-1 mb-2 text-sm text-gray-600">
+					<TeamsIcon width="18" height="18" />
+					{event.attendees.length}
+					{event.attendees.length === 1 ? " attendee" : " attendees"}
+				</span>
+				<p className="text-lg">{event.description}</p>
+				<div className="flex gap-2">
+					<Tag className="w-fit flex items-center gap-1">
+						<EventsIcon width="16" height="16" />
+						{event.startDate.toDateString()}
+					</Tag>
+					<Tag className="w-fit flex items-center gap-1">
+						<ClockIcon width="16" height="16" />
+						{event.startDate.toLocaleTimeString("en-US")}
+					</Tag>
+				</div>
+				<div className="flex gap-2 mt-4">
+					{isBoard && <ShowCodeButton secretCode={event.code} />}
+					<LinkButton
+						className="text-green-700 border-green-700"
+						href={`/events/${event.id}/join`}
+					>
+						Join the event
+					</LinkButton>
+				</div>
+			</div>
 		</div>
 	);
 }
