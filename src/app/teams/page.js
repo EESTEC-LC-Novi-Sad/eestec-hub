17c72d07fe517c9ae1f7dd9afe98cca29a669981
mihna123 <mihailonvojinovic@gmail.com>
2025-02-15 21:48:40 +0100

    Add a link to the new team page

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -1,12 +1,26 @@
 import { getAllTeams } from "@/services/team.service";
+import { auth } from "../../../auth";
 import Link from "next/link";
+import Card from "../components/Card";
 import Tag from "../components/Tag";
 import Button from "../components/Button";
+import LinkButton from "../components/LinkButton";
 
 export default async function TeamsPage() {
+	const session = await auth();
+	if (!session || !session.user) {
+		redirect("/login");
+	}
+	const isBoard = session.user.role === "board";
+
 	const teams = await getAllTeams();
 	return (
 		<div className="flex flex-col items-center md:mt-4">
+			{isBoard && (
+				<Card className="bg-gray-100 w-full md:w-8/12 flex flex-col items-center">
+					<LinkButton href="/teams/new">Create new team</LinkButton>
+				</Card>
+			)}
 			<div className="md:border border-gray-300 md:rounded w-full md:w-8/12 divide-y divide-gray-300">
 				{teams.map(mapTeamsToCards)}
 			</div>
