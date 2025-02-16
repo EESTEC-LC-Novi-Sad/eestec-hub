f79543c091d1cd35ed1d40a17137306e1221ff18
mihna123 <mihailonvojinovic@gmail.com>
2025-02-16 20:03:00 +0100

    Add disabled prop to Button component

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,8 +1,15 @@
-export default function Button({ children, className, onClick, type }) {
+export default function Button({
+	children,
+	className,
+	onClick,
+	type,
+	disabled,
+}) {
 	return (
 		<button
 			type={type}
 			onClick={onClick}
+			disabled={disabled ?? false}
 			className={`p-1 border border-solid border-gray-400 rounded 
            hover:bg-gray-200
            ${className}`}
