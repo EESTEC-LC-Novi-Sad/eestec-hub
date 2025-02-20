5448bf774e951a04e2d6f3b254dacabd7032c7ed
mihna123 <mihailonvojinovic@gmail.com>
2025-02-20 11:06:24 +0100

    Restrict applications page to board only

--- src/app/applications/page.jsx
+++ src/app/applications/page.jsx
@@ -1,10 +1,21 @@
+import { auth } from "../../../auth";
+import { redirect } from "next/navigation";
 import ApplicationsPageProjectsTab from "./ApplicationsPageProjectsTab";
 import ApplicationsPageTeamsTab from "./ApplicationsPageTeamsTab";
 
 export default async function ApplicationsPage({ searchParams }) {
-	const { tab } = searchParams;
-	if (tab === "teams") {
-		return <ApplicationsPageTeamsTab />;
-	}
-	return <ApplicationsPageProjectsTab />;
+    const session = await auth();
+
+    if (!session || !session.user) {
+        redirect("/login");
+    }
+    if (session.user.role !== "board") {
+        redirect("/");
+    }
+
+    const { tab } = searchParams;
+    if (tab === "teams") {
+        return <ApplicationsPageTeamsTab />;
+    }
+    return <ApplicationsPageProjectsTab />;
 }
