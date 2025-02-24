63ce41eb632bbd9b3e3b5da4457cf24424821471
mihna123 <mihailonvojinovic@gmail.com>
2025-02-24 16:20:27 +0100

    Upgrade dashboard page a little bit

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,48 @@
 import { redirect } from "next/navigation";
 import { auth } from "../../auth";
-import { getNumOfNotifications } from "@/services/user.service";
-import { getProjectsCount } from "@/services/project.service";
+import { getUserById } from "@/services/user.service";
+import { getAllEvents } from "@/services/events.service";
+import { getAllProjects } from "@/services/project.service";
 import Link from "next/link";
+import { mapEventToCard } from "./lib/utils";
 
 export default async function Home() {
 	const session = await auth();
 	if (!session || !session.user) {
 		redirect("/login");
 	}
-	const notifications = await getNumOfNotifications(session.user.id);
-	const projectCount = await getProjectsCount();
+	const projects = await getAllProjects();
+	const allEvents = await getAllEvents();
+	const user = await getUserById(session.user.id);
+	const upcomingEvents = allEvents.filter((e) => e.startDate >= new Date());
 
 	return (
-		<div>
-			<h1>Hello, {session.user.firstName}</h1>
-			<Link href="/notifications">My notifications: {notifications}</Link>
-			<br />
-			<Link href="/projects">Available projects: {projectCount}</Link>
+		<div className="flex justify-center">
+			<div className="w-full md:w-5/12 px-2">
+				<h1 className="text-3xl mt-2">Hello, {session.user.username}</h1>
+				<h1 className="text-lg mt-2">Your role: {user.role}</h1>
+				<h1 className="text-lg mt-2">Your points: {user.points ?? 0}</h1>
+				<h1 className="text-2xl mt-2">Upcoming events</h1>
+				{upcomingEvents.map((e) => {
+					return (
+						<Link key={e.id} href={`/events/${e.id}`}>
+							<p className="ml-2 text-blue-700 text-lg hover:underline mt-2">
+								<b>{e.name}</b>
+							</p>
+						</Link>
+					);
+				})}
+				<h1 className="text-2xl mt-2">Available projects</h1>
+				{projects.slice(0, 5).map((p) => {
+					return (
+						<Link key={p.id} href={`/projects/${p.id}`}>
+							<p className="ml-2 text-blue-700 text-lg hover:underline mt-2">
+								<b>{p.name}</b>
+							</p>
+						</Link>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
