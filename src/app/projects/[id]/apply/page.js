1921159b2e520b6f651ed376e7ccf27bbe4fa021
mihna123 <mihailonvojinovic@gmail.com>
2025-02-27 20:15:12 +0100

    Remove searchParams on the project apply page

--- src/app/projects/[id]/apply/page.js
+++ src/app/projects/[id]/apply/page.js
@@ -6,7 +6,7 @@ import CheckAllIcon from "@/app/icons/CheckAllIcon";
 import { Separator } from "@/app/lib/utils";
 import ApplyToProjectForm from "./ApplyToProjectForm";
 
-export default async function ApplyProjectPage({ params, searchParams }) {
+export default async function ApplyProjectPage({ params }) {
 	const project = await getProjectById(params.id);
 	if (!project) {
 		return (
@@ -15,37 +15,8 @@ export default async function ApplyProjectPage({ params, searchParams }) {
 			</div>
 		);
 	}
-	const { success, failure } = searchParams;
-	console.log(searchParams);
 	return (
 		<div className="flex justify-center">
-			{success !== undefined && (
-				<div className="flex items-center justify-center absolute top-0 z-50 w-full h-screen bg-gray-900/50">
-					<div className="bg-white w-72 p-4 rounded mb-48">
-						<p className="text-lg mb-4">
-							You have successfully applied to this project!
-						</p>
-						<div className="flex flex-row-reverse">
-							<LinkButton href="/projects">Go back to projects</LinkButton>
-						</div>
-					</div>
-				</div>
-			)}
-			{failure !== undefined && (
-				<div className="flex items-center justify-center absolute top-0 z-50 w-full h-screen bg-gray-900/50">
-					<div className="bg-white border-2 w-72 p-4 rounded mb-48">
-						<p className="text-md mb-4">
-							There has been an error during submission of your application,
-							please try again later.
-						</p>
-						<div className="flex flex-row-reverse">
-							<LinkButton href={`/projects/${params.id}/apply`}>
-								Okay
-							</LinkButton>
-						</div>
-					</div>
-				</div>
-			)}
 			<div className="w-full md:w-1/2 px-2">
 				<h1 className="text-2xl">
 					<b>Apply for project &quot;{project.name}&quot;</b>
